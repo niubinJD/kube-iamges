8f3485304d6d7967dbe407deba2a50e883a5d9de
niubin <247761426@qq.com>
2020-01-04 15:33:41 +0800

    as

---
+++ sync.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# 同步镜像到本地
+docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/coredns:1.6.5 \
+&& docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/coredns:1.6.5 k8s.gcr.io/coredns:1.6.5 \
+&& docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/etcd:3.4.3-0 \
+&& docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/etcd:3.4.3-0 k8s.gcr.io/etcd:3.4.3-0 \
+&& docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-apiserver:v1.17.0 \
+&& docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-apiserver:v1.17.0 k8s.gcr.io/kube-apiserver:v1.17.0 \
+&& docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-controller-manager:v1.17.0 \
+&& docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-controller-manager:v1.17.0 k8s.gcr.io/kube-controller-manager:v1.17.0 \
+&& docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-proxy:v1.17.0 \
+&& docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-proxy:v1.17.0 k8s.gcr.io/kube-proxy:v1.17.0 \
+&& docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-scheduler:v1.17.0 \
+&& docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-scheduler:v1.17.0 k8s.gcr.io/kube-scheduler:v1.17.0 \
+&& docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/pause:3.1 \
+&& docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/pause:3.1 k8s.gcr.io/pause:3.1 \
