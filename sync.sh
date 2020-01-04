1c6146d8868c0e0db3ea907bc02754020aa1f833
niubin <247761426@qq.com>
2020-01-04 16:16:09 +0800

    add finnel image

--- sync.sh
+++ sync.sh
@@ -15,3 +15,10 @@ docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/coredns:1.6.5 \
 && docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-scheduler:v1.17.0 k8s.gcr.io/kube-scheduler:v1.17.0 \
 && docker pull registry.cn-hangzhou.aliyuncs.com/kube-iamges/pause:3.1 \
 && docker tag registry.cn-hangzhou.aliyuncs.com/kube-iamges/pause:3.1 k8s.gcr.io/pause:3.1 \
+&& docker rmi registry.cn-hangzhou.aliyuncs.com/kube-iamges/coredns:1.6.5 \
+&& docker rmi registry.cn-hangzhou.aliyuncs.com/kube-iamges/etcd:3.4.3-0 \
+&& docker rmi registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-apiserver:v1.17.0 \
+&& docker rmi registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-controller-manager:v1.17.0 \
+&& docker rmi registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-proxy:v1.17.0 \
+&& docker rmi registry.cn-hangzhou.aliyuncs.com/kube-iamges/kube-scheduler:v1.17.0 \
+&& docker rmi registry.cn-hangzhou.aliyuncs.com/kube-iamges/pause:3.1
